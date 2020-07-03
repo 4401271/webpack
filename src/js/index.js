fa372c0ee4506e26a9308407b1440bbf273bf17f
xiangxiang <xiangxiang@qq.com>
2020-07-03 18:16:05 +0800

    基本功能

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import {add, sub} from './module1'
 import {mul, getName} from './module2'
 import module3 from './module3'
-//引入json 图片 css都类似对于默认暴露的引入
-// import data from '../json/data.json'
+// 引入json 图片 css都类似对于默认暴露的引入
+import data from '../json/data.json'
 // import '../less/demo.less'
 
 console.log(add(1,2))
@@ -12,7 +12,7 @@ console.log(getName())
 console.log(module3.name, module3.age)
 module3.setName('Rose')
 console.log(module3.name, module3.age)
-// console.log(data, typeof data)
+console.log(data, typeof data)
 
 setTimeout(()=>{
   console.log("1")
